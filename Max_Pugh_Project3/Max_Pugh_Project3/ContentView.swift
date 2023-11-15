ab8b098d4fdeacf6f1a29e87bc2a81090fa379c6
Max Pugh <mpugh@chapman.edu>
2023-11-15 14:20:13 -0800

    added project 3

---
+++ Max_Pugh_Project3/Max_Pugh_Project3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Max_Pugh_Project3
+//
+//  Created by Max Pugh on 11/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Max_Pugh_Project3/Max_Pugh_Project3/Max_Pugh_Project3App.swift
@@ -0,0 +1,17 @@
+//
+//  Max_Pugh_Project3App.swift
+//  Max_Pugh_Project3
+//
+//  Created by Max Pugh on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Max_Pugh_Project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
